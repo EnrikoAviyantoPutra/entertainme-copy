e39d886761002d70f7d2adfacb84cba0e337fc6b
EnrikoAviyantoPutra <enriko.aviyanto@gmail.com>
2021-03-14 00:25:41 +0700

    Finish Orchestrator-Express

---
+++ server/orchestrator-express/controllers/MovieController.js
@@ -0,0 +1,80 @@
+const axios = require('axios')
+const Redis = require('ioredis')
+const redis = new Redis()
+
+
+class MovieController {
+
+  static async findMovies(req, res) {
+    try {
+      const moviesCache = await redis.get("movies:data")
+      if (moviesCache) {
+        console.log('ini dari cache redis')
+        res.status(200).json(JSON.parse(moviesCache))
+        
+      }else{
+        const moviesData = await axios.get('http://localhost:4001/movies')
+        redis.set("movies:data", JSON.stringify(moviesData.data))
+        console.log('ini dari axios', moviesData.data)
+        res.status(200).json(moviesData.data)
+
+      } 
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+  static async findMoviesById(req, res) {
+    try {
+      const id = req.params.id
+      const moviesCache = await redis.get("movies:data")
+      const result = JSON.parse(moviesCache).filter(data => data._id === id)
+      // console.log(result)
+      return res.status(200).json(result)
+    }catch (err) {
+      console.log(err)
+    }
+  }
+  static async createMovies(req, res) {
+    try {
+      const newMovie = await req.body
+      console.log(newMovie)
+      await redis.del("movies:data")
+      const { data } = await axios.post('http://localhost:4001/movies',{newMovie})
+      res.status(201).json(data)
+    }catch (err) {
+      console.log(err)
+    }
+  }
+
+  static async updateMovie(req, res) {
+    try {
+      const id = await req.params.id
+      const updateMovie = await req.body
+      console.log(updateMovie)
+      await redis.del("movies:data")
+      const { data } = await axios.put(`http://localhost:4001/movies/${id}`,{updateMovie})
+      res.status(201).json(data)
+    }catch (err) {
+      console.log(err)
+    }
+  }
+
+
+
+
+  static async deleteMoviesById(req, res) {
+
+    try {
+      await redis.del("movies:data")
+      const id = req.params.id 
+      console.log(id)
+      const {data} = await axios.delete(`http://localhost:4001/movies/${id}`)
+      res.status(200).json(data)
+    }catch (err){
+      console.log(err)
+    }
+  }
+}
+
+module.exports = MovieController
--- server/orchestrator-express/routes/EntertainmeRoute.js
+++ server/orchestrator-express/routes/EntertainmeRoute.js
@@ -1,14 +1,21 @@
 const router = require('express').Router()
 
 const EntertainmeMovies = require('../controllers/EntertainmeMovies')
-
+const MovieController = require('../controllers/MovieController')
+const TvSeriesController = require('../controllers/SeriesController')
 
 router.get('/', EntertainmeMovies.find)
-router.get('/movies',EntertainmeMovies.findMovies)
-router.post('/movies', EntertainmeMovies.createMovies)
-router.get('/movies/:id', EntertainmeMovies.findMoviesById)
-router.put('/movies/:id', EntertainmeMovies.updateMovie)
-router.delete('/movies/:id', EntertainmeMovies.deleteMoviesById)
 
+router.get('/movies',MovieController.findMovies)
+router.post('/movies', MovieController.createMovies)
+router.get('/movies/:id', MovieController.findMoviesById)
+router.put('/movies/:id', MovieController.updateMovie)
+router.delete('/movies/:id', MovieController.deleteMoviesById)
+
+router.get('/tvseries',TvSeriesController.findSeries)
+router.post('/tvseries', TvSeriesController.createSerie)
+router.get('/tvseries/:id', TvSeriesController.findSerieById)
+router.put('/tvseries/:id', TvSeriesController.updateSerie)
+router.delete('/tvseries/:id', TvSeriesController.deleteSerieById)
 
 module.exports = router
